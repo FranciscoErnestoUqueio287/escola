ca87d4651057da386111bc24827ac72fdba517b7
Francisco Ernesto Uqueio <franciscoernsto287@gmail.com>
2020-11-12 18:17:40 -0800

    Update admin.py

--- mandela/admin.py
+++ mandela/admin.py
@@ -6,5 +6,6 @@ admin.site.register(aluno)
 admin.site.register(conversas)
 admin.site.register(trabalho)
 admin.site.register(comunicados)
+admin.site.register(directoria)
 admin.site.register(imagens)
 # Register your models here.
